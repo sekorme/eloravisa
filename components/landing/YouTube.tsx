f928da4704fe759c10b895cdb845059c56d918f8
Alex <asiedua4@uni.coventry.ac.uk>
2026-02-04 16:22:16 +0000

    youtube updated

--- components/landing/YouTube.tsx
+++ components/landing/YouTube.tsx
@@ -16,7 +16,7 @@ export function YouTube() {
           <iframe
             width="100%"
             height="100%"
-            src="https://www.youtube.com/embed/2VMSKMZApeM"
+            src="https://youtu.be/OhV11JYsiWw?si=m7tw5Y4j8_Bt2Wc7"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
